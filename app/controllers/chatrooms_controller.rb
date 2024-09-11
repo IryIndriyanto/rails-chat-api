dda2f95dcc8b6f7cbac3fd131ed3a6b081db44e6
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-09-12 00:06:35 +0700

    feat: add messages and user to be included to get chatroom by id

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,7 +10,7 @@ class ChatroomsController < ApplicationController
 
   # GET /chatrooms/1
   def show
-    render json: @chatroom
+    render json: @chatroom, include: { messages: { include: :user } }
   end
 
   # POST /chatrooms
